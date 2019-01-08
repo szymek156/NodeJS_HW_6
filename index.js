fd9662f213221caff27b121e5f344da03873f36e
Szymon Zimnowoda <szymon.zimnowoda@gmail.com>
2019-01-08 22:45:21 +0100

    refactored for cluster

--- index.js
+++ index.js
@@ -80,7 +80,17 @@ const CPUs = require("os").cpus().length
 
 if (cluster.isMaster) {
     for (let i = 0; i < CPUs; i++) {
-        cluster.fork();
+        let worker = cluster.fork();
+
+        worker.on("exit", (code, signal) => {
+            if (signal) {
+                console.log(`worker was killed by signal: ${signal}`);
+            } else if (code !== 0) {
+                console.log(`worker exited with error code: ${code}`);
+            } else {
+                console.log("worker success!");
+            }
+        });
     }
 }
 else {
